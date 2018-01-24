e46a9fd03e2437d476d66fb99166955c0513684e
mmcachran <mattmcachran@gmail.com>
2018-01-24 11:28:40 -0500

    Store save logic.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,25 @@
+const mongoose = require( 'mongoose' );
+const Store = mongoose.model( 'Store' );
+
 exports.homePage = (req, res) => {
     res.render( 'index' );
 };
 
 exports.addStore = ( req, res ) => {
     res.render( 'editStore', { title: 'Add Store' } );
+};
+
+exports.createStore = (req,res) => {
+    // Save POST data to Store model.
+    const store = new Store(req.body);
+
+    //Save the data in MongoDB.
+    store.save(function(err,store) {
+        if ( ! err ) {
+            console.log( 'It worked' );
+            res.redirect('/');
+        }
+    });
+
+    
 };
