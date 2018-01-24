dffe619f089568e768f5db8633acd282db591c03
mmcachran <mattmcachran@gmail.com>
2018-01-24 14:06:44 -0500

    Add stores route.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,11 +11,19 @@ exports.addStore = ( req, res ) => {
 
 exports.createStore = async (req,res) => {
     // Save POST data to Store model.
-    const store = new Store(req.body);
+    const store = await (new Store(req.body)).save();
 
-    // Save the store data.
-    await store.save();
+    // Show alert.
+    req.flash( 'success', `Successfully Created ${store.name}. Care to leave a review?` );
 
     // Log out a message.
-    res.redirect( '/' );
+    res.redirect( `/store/${store.slug}` );
+};
+
+exports.getStores = async (req,res) => {
+    // Query the database for a list of all stores.
+    const stores = await Store.find();
+
+    // Render the template.
+    res.render( 'stores', { title: 'Stores', stores } );
 };
