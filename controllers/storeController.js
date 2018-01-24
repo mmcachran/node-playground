1df23e453830c67403edbf06377f1be5c0ea4911
mmcachran <mattmcachran@gmail.com>
2018-01-24 12:18:17 -0500

    Updates for saving store data.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -9,17 +9,13 @@ exports.addStore = ( req, res ) => {
     res.render( 'editStore', { title: 'Add Store' } );
 };
 
-exports.createStore = (req,res) => {
+exports.createStore = async (req,res) => {
     // Save POST data to Store model.
     const store = new Store(req.body);
 
-    //Save the data in MongoDB.
-    store.save(function(err,store) {
-        if ( ! err ) {
-            console.log( 'It worked' );
-            res.redirect('/');
-        }
-    });
+    // Save the store data.
+    await store.save();
 
-    
+    // Log out a message.
+    res.redirect( '/' );
 };
