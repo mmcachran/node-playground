a76a85540c6272261e5677cd4e39413e3ae4ad02
mmcachran <mattmcachran@gmail.com>
2018-01-24 16:01:54 -0500

    Add autocomplete functionality for the address fields.

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,16 @@
+function autocomplete( input, latInput, lngInput ) {
+    // Bail early if no input on the page.
+    if ( ! input ) {
+        return;
+    }
+    
+    const dropdown = new google.maps.places.Autocomplete(input);
+
+    dropdown.addListener( 'place_changed', () => {
+        const place = dropdown.getPlace();
+
+        latInput.value = place.geometry.location.lat();
+        lngInput.value = place.geometry.location.lng();
+    });
+}
+export default autocomplete;
