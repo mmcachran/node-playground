f6ab1f58fd7271e532bba51f92a7b37f99d81226
mmcachran <mattmcachran@gmail.com>
2018-01-25 14:23:08 -0500

    Only allow authors of a store to modify it.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -76,12 +76,18 @@ exports.getStores = async (req,res) => {
     res.render( 'stores', { title: 'Stores', stores } );
 };
 
+const confirmOwner = (store, user) => {
+    if ( ! store.author.equals( user._id ) ) {
+        throw Error( 'You must own a store in order to edit it!');
+    }
+};
+
 exports.editStore = async ( req, res ) => {
     // Find the store given the ID.
     const store = await Store.findOne({ _id: req.params.id });
 
     // Confirm owner of the store.
-    // ToDo
+    confirmOwner( store, req.user );
 
     // Render the edit form so the user can update the store.
     res.render( 'editStore', { title: `Edit ${store.name}`, store } );
