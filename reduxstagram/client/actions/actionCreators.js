97f64c28d9dd353ce982ef69ab07b02e76be475b
mmcachran <mattmcachran@gmail.com>
2018-01-29 15:36:14 -0500

    Initial commit.

---
+++ reduxstagram/client/actions/actionCreators.js
@@ -0,0 +1,27 @@
+
+// Increment likes.
+export function increment( index ) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    };
+}
+
+// Add comment.
+export function addComment( postId, author, comment ) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        authod,
+        comment
+    };
+}
+
+// Remove comment
+export function removeComment( postId, index ) {
+    return {
+        type: 'REMOVE_COMMENT',
+        index,
+        postId
+    };
+}
